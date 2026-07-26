38e6c8941acdfd1594f07febde8ca6d2d2ccbf76
CloudyNimbus <74114256+itzKiwiSky@users.noreply.github.com>
2026-07-26 19:54:34 -0300

    espaco para voltar ao menu

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -9,4 +9,17 @@ k.scene("gameoverscene", () => {
     ])
     bg.scale.x = k.width() / bg.width
     bg.scale.y = k.height() / bg.height
+
+    k.add([
+        k.text("Aperte ESPACO para voltar ao menu", {
+            size: 25,
+        }),
+        k.pos(k.center().x, k.height() - 72),
+        k.anchor("center"),
+        k.color(k.WHITE),
+    ]);
+
+    k.onKeyPress("space", () => {
+        k.go("menuscene");
+    });
 });
